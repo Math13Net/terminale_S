319a1c075990c9c1ccb280f0b08b61c686e70590
Christie Vassilian <36983385+Math13Net@users.noreply.github.com>
2018-03-03 10:56:56 +0100

    Update TS_Cours_01.py

--- TS_Cours_01.py
+++ TS_Cours_01.py
@@ -1,6 +1,20 @@
 # TS_Cours_01
 # Chapitre 1 : Raisonnement par récurrence - Limite d'une suite
 
+def precision_valeur(limite,precision):
+# U_0 = 0.1 et U_{n+1} = 2*U_n(1-U_n)
+# Algorithme de précision ayant pour paramètre la limite et la précision
+# trouve le rang n tq abs(U_n-limite) < precision
+  U = 0.1
+  n = 0
+  while abs(U - limite) >= precision :
+    U = 2*U*(1 -U)
+    n += 1
+  print ("n = ",n)
+  print("U_n = ",U)
+  print (" |",U," - ",limite," | = ",abs(U -0.5) )
+
+
 def depassement_valeur(A):
 # U_0 = 2 et U_{n+1} = 4/3*U_n + 1
 # Algorithme de dépassement d'1 valeur A donnée
